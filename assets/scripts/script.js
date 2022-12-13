9b65946de36c446fabe437c35cf11941dfd5d676
rash <orashusedmund@gmail.com>
2022-12-13 17:20:32 +0100

    made the other part work

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,38 @@
+let arrayOfUsers = [
+  {name: "Rash Oracle", age: 20},
+  {name: "Nana Otto", age: 20},
+  {name: "Jr Jr", age: 21},
+  {name: "Edmund Son", age: 22},
+  {name: "Orashus F.", age: 17},
+  {name: "Luciernaga Ndi", age: 19},
+  {name: "Storm King", age: 16},
+  {name: "Brandy Jonathan", age: 16},
+  {name: "Marry Anne", age: 20},
+  {name: "Alaric Mboma", age: 11},
+  {name: "Shey Emma", age: 29},
+  {name: "Tata Malone", age: 20},
+  {name: "Abu Bea", age: 24},
+  {name: "Charles Peterson", age: 24},
+  {name: "Mr. Gaston", age: 31},
+  {name: "Uncle Leo", age: 29},
+  {name: "Mme Faith", age: 28},
+  {name: "Gran Humphry", age: 20},
+  {name: "Mr Array", age: 26},
+  {name: "Ashley Jones", age: 15},
+  {name: "Kilian Mbappe", age: 23},
+  {name: "Christiano Ronaldo", age: 37},
+  {name: "Leonel Messi", age: 36},
+  {name: "Zlatan Ibrahim", age: 40},
+  {name: "Lebron James", age: 37},
+  {name: "Jamel Morant", age: 21},
+  {name: "Steve Dogllas", age: 19},
+  {name: "Steve Dogllas", age: 19},
+  {name: "Steve Dogllas", age: 19},
+  {name: "Steve Dogllas", age: 19},
+  {name: "Senior Dev", age: 19},
+  {name: "Jango Kelmith", age: 19},
+]
+
 const form = document.querySelector('form');
 const userContainers = document.querySelector('.all-users');
 
@@ -64,40 +99,41 @@ function shouldResolve() {
   return Math.random() < 0.85;
 }
 
-/* function searchUsers(name, age) {
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve ()) {
-        resolve(
-          users.filter(
-            (user) => 
-            (!name || compareNames(user.name, name)) &&
-            (!age || user.age === age)
-          )
-        );
-      }else {
-        reject([]);
-      }
-    }, 2000);
-  });
-}
 
-function renderMessage(message) {
-  return `<div class="message">${message}</div>`;
-}
+// function searchUsers(name, age) {
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldResolve ()) {
+//         resolve(
+//           arrayOfUsers.filter(
+//             (user) => 
+//             (!name || searchUsers(user.name, name)) &&
+//             (!age || user.age === age)
+//           )
+//         );
+//       }else {
+//         reject([]);
+//       }
+//     }, 2000);
+//   });
+// }
 
-userContainers.innerHTML = displayUsers(users);
+// function renderMessage(message) {
+//   return `<div class="message">${message}</div>`;
+// }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  userContainers.innerHTML = renderMessage("searching users...");
-  searchUsers(e.target.name.value, +e.target.age.value)
-  .then((result) => {
-    userContainers.innerHTML = displayUser(result);
-  })
-  .catch(e) = > {
-    userContainers.innerHTML = renderMessage(
-      "Error loading users! Please try again"
-    );
-  };
-}); */
+// // userContainers.innerHTML = displayUsers(arrayOfUsers);
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   userContainers.innerHTML = renderMessage("searching users...");
+//   searchUsers(e.target.name.value, +e.target.age.value)
+//   .then((result) => {
+//     userContainers.innerHTML = displayUser(result);
+//   })
+//   .catch((e) => {
+//     userContainers.innerHTML = renderMessage(
+//       "Error loading users! Please try again"
+//     );
+//   });
+// });
