0a5c3b9c7ebe6049d38d18736fdba92892fa4387
rash <orashusedmund@gmail.com>
2023-06-20 10:46:25 +0100

    fixed linters

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,62 +1,60 @@
-import data from "../data/data.js";
-import { deleteUser, showOptions } from "./utils.js";
+import data from '../data/data.js'
+import { deleteUser, resetAll } from './utils.js'
 
-const arrayOfUsers = data;
+const arrayOfUsers = data
 
-const form = document.querySelector("form");
-const userContainers = document.querySelector(".all-users");
-const nameInput = document.querySelector("#search-name");
-const ageInput = document.querySelector("#search-age");
+const form = document.querySelector('form')
+const userContainers = document.querySelector('.all-users')
+const nameInput = document.querySelector('#search-name')
+const reset = document.querySelector('#reset')
 
-const options = document.querySelector(".options");
+reset.addEventListener('click', resetAll)
 
-options.addEventListener("click", () => showOptions());
+function displayUser ({ age, name }, acronym, highlighted) {
+  const userDiv = document.createElement('div')
+  userDiv.className = 'user'
 
-function displayUser({ age, name }, acronym, highlighted) {
+  const userProfile = document.createElement('div')
+  userProfile.className = 'user-profile'
+  userProfile.innerHTML = `${acronym}`
 
-  const userDiv = document.createElement("div");
-  userDiv.className = "user";
+  const container = document.createElement('div')
+  const text = document.createElement('div')
+  text.className = 'text'
 
-  const userProfile = document.createElement("div");
-  userProfile.className = "user-profile";
-  userProfile.innerHTML = `${acronym}`;
-
-  const container = document.createElement("div");
-  const text = document.createElement("div");
-  text.className = "text";
-
-  const userName = document.createElement("p");
-  userName.className = "user-name";
+  const userName = document.createElement('p')
+  userName.className = 'user-name'
   userName.innerHTML = `${
     nameInput.value.length === 0 ? name : getHilight(name, highlighted)
-  }`;
+  }`
 
-  const userAge = document.createElement("p");
-  userAge.className = "user-age";
-  userAge.innerHTML = `${age} year${age > 1 ? "s" : ""}`;
+  const userAge = document.createElement('p')
+  userAge.className = 'user-age'
+  userAge.innerHTML = `${age} year${age > 1 ? 's' : ''}`
 
-  text.appendChild(userName);
-  text.appendChild(userAge);
+  text.appendChild(userName)
+  text.appendChild(userAge)
 
-  const removeBtn = document.createElement("button");
-  removeBtn.className = "remove-user";
-  removeBtn.innerHTML = "delete";
-  removeBtn.addEventListener("click", () => deleteUser(name));
+  const removeBtn = document.createElement('button')
+  removeBtn.className = 'remove-user'
+  removeBtn.innerHTML = 'delete'
+  removeBtn.title = `delete ${name}`
+  removeBtn.addEventListener('click', () => deleteUser(name))
 
-  container.appendChild(text);
-  container.appendChild(removeBtn);
+  container.appendChild(text)
+  container.appendChild(removeBtn)
 
-  userDiv.appendChild(userProfile);
-  userDiv.appendChild(container);
+  userDiv.appendChild(userProfile)
+  userDiv.appendChild(container)
 
-  userContainers.appendChild(userDiv);
+  userContainers.appendChild(userDiv)
 }
 
 const getHilight = (name, keyWord) => {
   for (const i in name) {
-    const keyWordLength = parseInt(keyWord.length) + parseInt(i);
+    const keyWordLength = parseInt(keyWord.length) + parseInt(i)
     if (name.trim().toLowerCase() === keyWord.trim().toLowerCase()) {
-      return name;
+      return name
     } else if (
       keyWord.length === 1 &&
       name.charAt(i).toLowerCase() === keyWord.charAt(0).toLowerCase()
@@ -64,7 +62,7 @@ const getHilight = (name, keyWord) => {
       return `${name.slice(0, i)}<span class="highlight">${name.slice(
         i,
         keyWordLength
-      )}</span>${name.slice(keyWordLength, name.length)}`;
+      )}</span>${name.slice(keyWordLength, name.length)}`
     } else if (
       keyWord.length > 1 &&
       name.charAt(i).toLowerCase() +
@@ -74,86 +72,86 @@ const getHilight = (name, keyWord) => {
       return `${name.slice(0, i)}<span class="highlight">${name.slice(
         i,
         keyWordLength
-      )}</span>${name.slice(keyWordLength, name.length)}`;
+      )}</span>${name.slice(keyWordLength, name.length)}`
     }
-  }
-};
-
-const getInitials = (name) => {
-  const arra = [...name];
-  let final = arra[0];
-  for (let j = 0; j < arra.length; j++) {
-    if (arra[j].includes(" ") && typeof (arra[j + 1] === String)) {
-      final += arra[j + 1];
-    }
-  }
-  return final;
-};
-
-export function displayUsers(persons, highlight) {
-  // for loop method of displaying
-  let firsTwoLetters = "";
-  for (let i = 0; i < persons.length; i++) {
-    firsTwoLetters = getInitials(persons[i].name);
-    displayUser(persons[i], firsTwoLetters, highlight);
   }
 }
 
-function searchUsersLoop(name, age) {
+const getInitials = (name) => {
+  const arra = [...name]
+  let final = arra[0]
+  for (let j = 0; j < arra.length; j++) {
+    if (arra[j].includes(' ') && typeof (arra[j + 1] === String)) {
+      final += arra[j + 1]
+    }
+  }
+  return final
+}
+
+export function displayUsers (persons, highlight) {
+  // for loop method of displaying
+  let firsTwoLetters = ''
+  for (let i = 0; i < persons.length; i++) {
+    firsTwoLetters = getInitials(persons[i].name)
+    displayUser(persons[i], firsTwoLetters, highlight)
+  }
+}
+
+function searchUsersLoop (name, age) {
   // for loop method of searching
-  const results = [];
+  const results = []
   for (let i = 0; i < arrayOfUsers.length; i++) {
     if (
       (!name ||
         arrayOfUsers[i].name.toLowerCase().includes(name.toLowerCase())) &&
       (!age || arrayOfUsers[i].age === age)
     ) {
-      results.push(arrayOfUsers[i]);
+      results.push(arrayOfUsers[i])
     }
   }
 
-  displayUsers(results, name);
+  displayUsers(results, name)
 }
 
 // boundary line
 
-function searchUsers(name, age) {
+function searchUsers (name, age) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      let bool = false;
+      let bool = false
       for (let i = 0; i < arrayOfUsers.length; i++) {
         if (
           (!name ||
             arrayOfUsers[i].name.toLowerCase().includes(name.toLowerCase())) &&
           (!age || arrayOfUsers[i].age === age)
         ) {
-          bool = true;
+          bool = true
         }
       }
       if (bool) {
-        resolve(searchUsersLoop(name, age));
+        resolve(searchUsersLoop(name, age))
       } else {
-        reject("nothing here");
+        reject(new Error('An error occured'))
       }
-    }, 1500);
-  });
+    }, 1500)
+  })
 }
 
-function renderMessage(message) {
-  return `<div class="message">${message}</div>`;
+function renderMessage (message) {
+  return `<div class="message">${message}</div>`
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  userContainers.innerHTML = renderMessage("searching users...");
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  userContainers.innerHTML = renderMessage('searching users...')
   searchUsers(e.target.name.value, +e.target.age.value) // this another way to pass these inputs
     .catch(() => {
       userContainers.innerHTML = renderMessage(
-        "Error! no user matches your search, please try again"
-      );
-    });
+        'Error! no user matches your search, please try again'
+      )
+    })
 });
 
 (function () {
-  displayUsers(arrayOfUsers);
-})();
+  displayUsers(arrayOfUsers)
+})()
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,35 +1,25 @@
-import arrayOfUsers from "../data/data.js";
-import { displayUsers } from "./script.js";
+import { resetData } from '../data/data.js'
+import { displayUsers } from './script.js'
 
 const getSessionData = () =>
-  JSON.parse(sessionStorage.getItem("users")) || arrayOfUsers;
+  JSON.parse(window?.sessionStorage.getItem('users')) || resetData
 
 const saveToSession = (update) =>
-  sessionStorage.setItem("users", JSON.stringify(update));
+  window?.sessionStorage.setItem('users', JSON.stringify(update))
 
 export const deleteUser = (name) => {
-  const newUserData = getSessionData().filter((user) => user.name !== name);
+  const newUserData = getSessionData().filter((user) => user.name !== name)
 
-  document.querySelector(".all-users").innerHTML = "";
+  document.querySelector('.all-users').innerHTML = ''
 
-  displayUsers(newUserData);
+  displayUsers(newUserData)
 
-  saveToSession(newUserData);
-};
+  saveToSession(newUserData)
+}
 
-export const showOptions = () => {
-  const options = document.querySelector(".select");
-  const overlay = document.querySelector(".overlay");
-  options.classList.toggle("active");
-  overlay.classList.toggle("active");
+export const resetAll = () => {
+  window.sessionStorage.removeItem('users')
+  saveToSession(resetData)
 
-  overlay.addEventListener("click", () => {
-    options.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-
-  options.addEventListener("click", () => {
-    saveToSession(arrayOfUsers);
-    displayUsers(getSessionData());
-  });
-};
+  displayUsers(resetData)
+}
